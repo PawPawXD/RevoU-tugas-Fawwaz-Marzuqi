35e875c191375751454c0b027bd9ceabe9e944d6
Fawwaz M <162989478+PawPawXD@users.noreply.github.com>
2024-03-14 21:56:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calculate() {
+        let weightInput = document .getElementById('weight-input').value;
+        console.log(weightInput);
+        let heightInput = document .getElementById('height-input').value;
+        console.log(heightInput);
+
+        if (weightInput == '' || height.value=='') {
+            alert('Harap isi berat badan Anda')
+        } else {
+            console.log('Ada Isi')
+        }
+
+}
